f9c2b31ce8f248d3c1af05753ecef522e2e048d7
Crista Chiangco <82898714+cristachiangco@users.noreply.github.com>
2021-06-16 13:52:47 -0700

    20210616 crista

--- resources/views/bc_chat.blade.php
+++ resources/views/bc_chat.blade.php
@@ -16,10 +16,7 @@
         <div id="cb_container_1">
             <a href="/" id="title"><h1>Pasig City Online Teleconsultation</h1></a>
             <div id="cb_cont">
-
                 <div id="cbcont_inner">
-                    <h2>Hi and Welcome ~</h2>
-
                     <div id="convo">
                         <p id=cb>hkasdfghjk</p>
                         <p id="pa">saadretfghyui</p>
@@ -30,8 +27,15 @@
                         <p id=cb>hkasdfghjk</p>
                         <p id="pa">saadretfghyui</p>
                     </div>
-
                 </div>
+            </div>
+            <div id="textbox">
+                <table id="textbox_tbl">
+                    <tr>
+                        <td id="txtbx"><input id="tb" type="text" placeholder="Message"></input></td>
+                        <td id="tb_btn"><img id="send" src="/icons/send.png"></td>
+                    </tr>
+                </table>
             </div>
         </div>
     </body>
