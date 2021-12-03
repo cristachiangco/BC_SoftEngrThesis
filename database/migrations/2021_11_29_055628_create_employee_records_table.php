8231c154247d7c42303b1abf6e5ab314a5409419
Crista Chiangco <82898714+cristachiangco@users.noreply.github.com>
2021-12-03 16:33:00 +0800

    DBs

--- database/migrations/2021_11_29_055628_create_employee_records_table.php
+++ database/migrations/2021_11_29_055628_create_employee_records_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateEmployeeRecordsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('employee_records', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('middle_int');
             $table->timestamps();
-            $table->string('home_address');
         });
     }
 
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('employee_records');
     }
 }
